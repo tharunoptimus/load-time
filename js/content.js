3a06e31ea2c105096e264badb8dcbe912ddfbcd1
tharunoptimus <tharunoptimus@outlook.com>
2023-03-03 06:27:07 +0530

    Added the basic domContentLoaded timing

--- js/content.js
+++ js/content.js
@@ -1,1 +1,5 @@
-console.log("from content")
+let performanceValues = {}
+
+performanceValues["deprecatedDomContentLoaded"] =
+	window.performance.timing.domContentLoadedEventEnd -
+	window.performance.timing.navigationStart
